f5204bf11e37ed9be9e2e15bbec8b45c96f97014
Pinak <mazumdar.pinak@gmail.com>
2025-07-08 21:18:58 +0530

    add files

--- backstage/blog-app/packages/backend/src/index.ts
+++ backstage/blog-app/packages/backend/src/index.ts
@@ -1,55 +1,21 @@
-/*
- * Hi!
- *
- * Note that this is an EXAMPLE Backstage backend. Please check the README.
- *
- * Happy hacking!
- */
+import winston from 'winston';
+import { createRootLogger } from '@backstage/backend-common';
+import 'winston-daily-rotate-file';
 
-import { createBackend } from '@backstage/backend-defaults';
+const logFileTransport = new winston.transports.File({
+  filename: 'backstage.log',
+  dirname: 'logs', // writes to packages/backend/logs/backstage.log
+  level: 'info', // capture info and higher levels
+});
 
-const backend = createBackend();
+const logger = createRootLogger();
+logger.add(logFileTransport);
 
-backend.add(import('@backstage/plugin-app-backend'));
-backend.add(import('@backstage/plugin-proxy-backend'));
-backend.add(import('@backstage/plugin-scaffolder-backend'));
-backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
-backend.add(import('@backstage/plugin-techdocs-backend'));
+const logRotateTransport = new winston.transports.DailyRotateFile({
+  dirname: 'logs',
+  filename: 'backstage-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  maxFiles: '14d', // keep logs for 2 weeks
+});
+logger.add(logRotateTransport);
 
-// auth plugin
-backend.add(import('@backstage/plugin-auth-backend'));
-// See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
-backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
-// See https://backstage.io/docs/auth/guest/provider
-
-// catalog plugin
-backend.add(import('@backstage/plugin-catalog-backend'));
-backend.add(
-  import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
-);
-
-// See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
-backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
-
-// permission plugin
-backend.add(import('@backstage/plugin-permission-backend'));
-// See https://backstage.io/docs/permissions/getting-started for how to create your own permission policy
-backend.add(
-  import('@backstage/plugin-permission-backend-module-allow-all-policy'),
-);
-
-// search plugin
-backend.add(import('@backstage/plugin-search-backend'));
-
-// search engine
-// See https://backstage.io/docs/features/search/search-engines
-backend.add(import('@backstage/plugin-search-backend-module-pg'));
-
-// search collators
-backend.add(import('@backstage/plugin-search-backend-module-catalog'));
-backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
-
-// kubernetes
-backend.add(import('@backstage/plugin-kubernetes-backend'));
-
-backend.start();
